522a4e187f48e1e5071cf3ab75c024ec44715c12
Abhinand C <abhinandc.ac@gmail.com>
2025-10-09 10:40:41 +0530

    feat: Add support for python 3.14

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,8 @@
 from nox import parametrize, session
 
 
-@session(python=["3.9", "3.10", "3.11", "3.12"])
-@parametrize("graphene", ("3.0", "3.1", "3.2", "3.3"))
+@session(python=["3.9", "3.10", "3.11", "3.12", "3.13", "3.14"])
+@parametrize("graphene", ("3.0", "3.1", "3.2", "3.3", "3.4"))
 def tests(session, graphene):  # noqa
     session.install(f"graphene=={graphene}")
     session.install("pytest", ".")
